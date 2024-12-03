32c00dfab72db522e91786b8a3dad30a988936f2
DavidFezes <davidpaulino702@gmail.com>
2024-12-02 23:35:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'PC',
+    'Melhor jogo?',
+    'Valorant'
+)
+
+criaCartao(
+    'Filmes',
+    'Melhor filme',
+    'Projeto X'
+)
+
+criaCartao(
+    'Escola',
+    'Melhor prof?',
+    'Eduarda'
+)
+
+criaCartao(
+    'Futebol',
+    'Melhor time',
+    'Santos'
+)
